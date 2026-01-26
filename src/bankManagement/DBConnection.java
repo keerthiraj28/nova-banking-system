2dc651e09b25a88ef33e1b82d5837a86fe3a4110
keerthiraj28 <rajkeerthi1223@gmail.com>
2026-01-26 22:32:54 +0530

    refactor: externalize database credentials from DBConnection

--- src/bankManagement/DBConnection.java
+++ src/bankManagement/DBConnection.java
@@ -1,21 +1,39 @@
 package bankManagement;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String URL = System.getenv("DB_URL");
-
-    private static final String USER = System.getenv("DB_USER");
-
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
 
     static {
         try {
+            // Load MySQL driver
             Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Load db.properties from classpath
+            Properties props = new Properties();
+            InputStream is = DBConnection.class
+                    .getClassLoader()
+                    .getResourceAsStream("db.properties");
+
+            if (is == null) {
+                throw new RuntimeException("db.properties not found in classpath");
+            }
+
+            props.load(is);
+
+            URL = props.getProperty("db.url");
+            USER = props.getProperty("db.username");
+            PASSWORD = props.getProperty("db.password");
+
         } catch (Exception e) {
-            throw new RuntimeException("MySQL Driver not loaded");
+            throw new RuntimeException("Failed to load database configuration", e);
         }
     }
 
